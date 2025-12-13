9becfe790a7bbd669129e6e9ef99cafdee1139f8
Sadman Islam Chowdhury Samin <sammyshade664@gmail.com>
2025-12-13 12:00:23 +0600

    Added Login in MVC

---
+++ MVC/ERP/HR/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using HR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HR.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            UserDAL dal = new UserDAL();
+            var user = dal.ValidateUser(username, password);
+
+            if (user != null)
+            {
+                Session["UserID"] = user.ID;
+                Session["Username"] = user.Username;
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid username or password";
+            return View();
+        }
+
+        // Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+            return RedirectToAction("Login", "User");
+        }
+    }
+}
---
+++ MVC/ERP/HR/Models/UserDAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace HR.Models
+{
+    public class UserDAL
+    {
+        private readonly string _connectionString =
+            ConfigurationManager.ConnectionStrings["dberpbatch2connection"].ToString();
+
+        public User ValidateUser(string username, string password)
+        {
+            const string sql = @"SELECT ID, Username 
+                                 FROM [dbo].[User] 
+                                 WHERE Username = @Username AND Password = @Password";
+
+            using (var con = new SqlConnection(_connectionString))
+            using (var cmd = new SqlCommand(sql, con))
+            {
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                con.Open();
+                var reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    return new User
+                    {
+                        ID = (int)reader["ID"],
+                        Username = reader["Username"].ToString()
+                    };
+                }
+            }
+            return null;
+        }
+    }
+}
